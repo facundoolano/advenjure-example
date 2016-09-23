e4a295cd41975538311f4d0d7f94789caba09c42
Facundo Olano <facundo.olano@gmail.com>
2016-09-22 22:39:13 -0300

    example working in cljs

--- project.clj
+++ project.clj
@@ -3,8 +3,20 @@
   :url "https://github.com/facundoolano/advenjure-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.1.4"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/core.async "0.2.391"]
                  [advenjure "0.2.0"]]
+  :cljsbuild
+    {:builds
+     [{:source-paths ["src"]
+       :compiler {:output-to "main.js"  ; default: target/cljsbuild-main.js
+                  :output-dir "out"
+                  :main example.core
+                  :optimizations :none
+                  :source-map true
+                  :pretty-print true}}]}
   :main ^:skip-aot example.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
