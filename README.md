66a2b1955685e660260f976d63c86406fa2b0df8
Facundo Olano <facundo.olano@gmail.com>
2016-10-27 21:45:52 -0300

    updated figwheel configuration and readme

--- README.md
+++ README.md
@@ -14,10 +14,18 @@ To run the example game on the terminal:
 lein run
 ```
 
-To run the example game on the browser:
+To run the example game on the browser, using figwheel for development:
 
 ```
 lein figwheel dev
 ```
 
 Then open `http://localhost:3449`.
+
+To compile the optimized JavaScript to run in production:
+
+```
+lein cljsbuild once main
+```
+
+Then open `resources/public/index.html`.
--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             [lein-figwheel "0.5.4-7"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
-                 [advenjure "0.6.2"]]
+                 [advenjure "0.6.3"]]
   :cljsbuild
     {:builds
      {:main {:source-paths ["src"]
@@ -18,8 +18,10 @@
                         :optimize-constants true
                         :static-fns true}}
 
-      :dev {:source-paths ["src"]
-            :figwheel true
+      :dev {:source-paths ["src" "checkouts/advenjure/src"]
+            :figwheel {:load-warninged-code true
+                       :before-jsload "advenjure.ui.input/figwheel-cleanup"}
+
             :compiler {:output-to "resources/public/js/main.js"
                        :output-dir "resources/public/js/out"
                        :main example.core
